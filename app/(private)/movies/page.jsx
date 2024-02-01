ee630fe54c78688757e9051718a4c0d53388aaae
“red” <karatasmert255@gmail.com>
2024-02-01 22:20:52 +0300

    Add HeroSection and VideoSection components

--- app/(private)/movies/page.jsx
+++ app/(private)/movies/page.jsx
@@ -1,7 +1,10 @@
+import { getMovies } from "@/helpers/movieFunctions";
 import React from "react";
 
-const Movies = () => {
-  return <div>Movies</div>;
+const Movies = async () => {
+  const movies = await getMovies("now_playing");
+  console.log(movies);
+  return <></>;
 };
 
 export default Movies;
